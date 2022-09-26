469ba3efdb2d93900b7204f12d43dd18f1053101
DeyanRomanov <deyan.romanov@gmail.com>
2022-09-26 08:28:26 +0300

    Add UserSingIn

---
+++ app.py
@@ -0,0 +1,68 @@
+import enum
+
+from decouple import config
+from flask import Flask, request
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
+from marshmallow import Schema, fields
+
+app = Flask(__name__)
+db_user = config('DB_USER')
+db_password = config("DB_PASSWORD")
+db_name = config("DB_NAME")
+db_port = config("DB_PORT")
+db_host = config("DB_HOST")
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
+db = SQLAlchemy(app)
+api = Api(app)
+migrate = Migrate(app, db)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    full_name = db.Column(db.String(255), nullable=False)
+    phone = db.Column(db.Text)
+    create_on = db.Column(db.DateTime, server_default=func.now())
+    updated_on = db.Column(db.DateTime, onupdate=func.now())
+
+
+class ColorEnum(enum.Enum):
+    pink = "pink"
+    black = "black"
+    white = "white"
+    yellow = "yellow"
+
+
+class SizeEnum(enum.Enum):
+    xs = "xs"
+    s = "s"
+    m = "m"
+    l = "l"
+    xl = "xl"
+    xxl = "xxl"
+
+
+class Clothes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    color = db.Column(db.Enum(ColorEnum), default=ColorEnum.white, nullable=False)
+    size = db.Column(db.Enum(SizeEnum), default=SizeEnum.s, nullable=False)
+    photo = db.Column(db.String(255), nullable=False)
+    create_on = db.Column(db.DateTime, server_default=func.now())
+    updated_on = db.Column(db.DateTime, onupdate=func.now())
+
+
+class UserSingIn(Resource):
+    def post(self):
+        data = request.get_json()
+        user = User(**data)
+        db.session.add(user)
+        db.sessiom.commit()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
